82b6e5d2a1f10a949bd704ce3e0e5d8e0f5e61c7
acalpixca <acalpixca@gmail.com>
2017-02-17 14:30:35 +0100

    Add files via upload

---
+++ src/questions.js
@@ -0,0 +1,273 @@
+module.exports = [
+    {
+        "In what country is Barcelona located?": [
+            "Spain",
+            "France",
+            "Italy",
+            "Mexico"
+        ],
+        "Explanation": "Barcelona is the second city in Spain, although it's a little known fact that there's another city with the same name in Venezuela"
+    },
+    {
+        "What's the population of Barcelona?": [
+            "1.6 million people",
+            "3 million people",
+            "500000 people",
+            "2.5 million people"
+        ],
+        "Explanation": "The city's growth is limited by two rivers, the sea and the mountain, so it's not huge"
+    },
+    {
+        "Which ancient civilization founded the city?": [
+            "The Carthaginians",
+            "The Greeks",
+            "The Phoenicians",
+            "The Romans"
+        ],
+        "Explanation": "Everyone thinks it was the Romans, when in fact the Roman name for Barcelona means New Barca, the Carthaginian general that founded the city"
+    },
+    {
+        "Which sea laps the shores of city?": [
+            "The Mediterranean sea",
+            "The Aegean sea",
+            "The Red sea",
+            "The Caribbean sea"
+        ],
+        "Explanation": "Mediterranean means the center of Earth in Latin, that's how important this sea was to the Romans"
+    },
+    {
+        "When was the city of Barcelona founded?": [
+            "in the 3rd century BC",
+            "in 1590",
+            "in the 9th century BC",
+            "It is unknown"
+        ],
+        "Explanation": "Indeed, the city is over 2000 years old"
+
+    },
+    {
+        "How many soccer clubs from Barcelona play in Spain's top division La Liga ?": [
+            "2 teams",
+            "1 team",
+            "3 teams",
+            "No teams"
+        ],
+        "Explanation": "Valencia has as many teams. Both are beaten by Madrid, which in 2016 boasts 4 teams in top division"
+    },
+    {
+        "How often does it snow in Barcelona, on average?": [
+            "Once every 5 years",
+            "Never",
+            "Every winter",
+            "Once every 20 years"
+        ],
+        "Explanation": "It's a rare event and not so much to witness, as the city grinds to a halt"
+    },
+    {
+        "What kind of climate does Barcelona have?": [
+            "Mediterranean",
+            "Atlantic",
+            "Continental",
+            "Tropical"
+        ],
+        "Explanation": "With hot, sticky summers and mild but humid winters, spring and fall are the seasons you want to aim for"
+    },
+    {
+        "What's the average annual temperature during the day?": [
+            "21 degrees Celsius",
+            "32 degrees Celsius",
+            "15 degrees Celsius ",
+            "27 degrees Celsius"
+        ],
+        "Explanation": "Check the weather forecast before you visit the city and pack accordingly. You may find it colder than expected if not visiting in summer"
+    },
+    {
+        "What can you find in the Gothic quarter?": [
+            "All options are true",
+            "Fast food shops",
+            "Medieval buildings",
+            "Street performers"
+        ],
+        "Explanation": "The Gothic quarter is the true gem of the city as not so many places have over 2000 years of history and such a lively atmosphere"
+    },
+    {
+        "What is Modernista architecture?": [
+            "The same as Art Nouveau",
+            "The distinct architecture of the two towers by the sea",
+            "Only buildings designed by Gaudi can be called Modernista",
+            "A commercial brand for souvenirs"
+        ],
+        "Explanation": "Gaudi is the most famous architect of this style, but you may also want to check out other buildings by Mila i Fontanals"
+    },
+    {
+        "Which is Barcelona's main tourist attraction, according to number of visitors?": [
+            "Sagrada Familia",
+            "Soccer stadium",
+            "Olympic village",
+            "The cathedral"
+        ],
+        "Explanation": "There are always long queues there, so you better book your tickets online"
+    },
+    {
+        "Barcelona has plenty of museums, but which one is the most visited?": [
+            "Football Club Barcelona museum",
+            "Picasso Museum",
+            "Maritime Museum",
+            "National Museum of Art"
+        ],
+        "Explanation": "The city lacks first class museums such as The Louvre in Paris or The British Museum in London, so this is hardly a surprise"
+    },
+    {
+        "Which city in Spain has the most important art museum?": [
+            "Madrid",
+            "Barcelona",
+            "Malaga",
+            "Seville"
+        ],
+        "Explanation": "El Prado museum is on par with London's National Gallery. You shouldn't miss it while in Spain"
+    },
+    {
+        "Which one is not a beach in Barcelona?": [
+            "Copacabana",
+            "Sant Sebastia",
+            "Barceloneta",
+            "Mar Bella"
+        ],
+        "Explanation": "Copacabana is in Brazil. All the others can be reached by Barcelona's public transport"
+    },
+    {
+        "How many parks are in the city?": [
+            "68",
+            "34",
+            "5",
+            "12"
+        ],
+        "Explanation": "The biggest park is Ciutadella and it's small if compared with other European cities. In return, there are many smaller parks"
+    },
+    {
+        "17.4 per cent of the city's inhabitants are not Spanish. Which is the most representative foreign nationality in Barcelona?": [
+            "Pakistan",
+            "Italy",
+            "China",
+            "Ecuador"
+        ],
+        "Explanation": "Our Pakistani neighbours are very entrepreneurial and they have opened many shops in the last decade"
+    },
+    {
+        "What's the most popular religion in Barcelona?": [
+            "Roman Catholic",
+            "Jedi",
+            "Atheism",
+            "Islam"
+        ],
+        "Explanation": "Even though Barcelona is one of the least religious cities in Spain, and mass attendance is quite low, many locals still see themselves as Catholics"
+    },
+    {
+        "How many international visitors did the city have in 2011?": [
+            "5.5 millions",
+            "2 millions",
+            "7.5 millions",
+            "1 million"
+        ],
+        "Explanation": "For each inhabitant, the city receives over three visitors in a year"
+    },
+    {
+        "How many districts does the city have?": [
+            "10",
+            "20",
+            "5",
+            "15"
+        ],
+        "Explanation": "During your visit you will spend most of your time in the Ciutat Vella and Eixample districts, but there are eight more"
+    },
+    {
+        "How many official languages are spoken in Barcelona?": [
+            "2 languages",
+            "1 language",
+            "3 languages",
+            "There's not such a thing as an official language"
+        ],
+        "Explanation": "Spanish and Catalan are both official languages and you will see most signs written in both languages, sometimes in English too"
+    },
+    {
+        "How many times has Barcelona hosted the Olympic Games?": [
+            "1 time",
+            "None",
+            "Twice",
+            "3 times"
+        ],
+        "Explanation": "It was in 1992"
+    },
+    {
+        "When did the Barcelona Olympic Games take place?": [
+            "1992",
+            "1996",
+            "2000",
+            "1988"
+        ],
+        "Explanation": "The Barcelona games came after Seoul in Korea and before Atlanta in the United States"
+    },
+    {
+        "The Barcelona Marathon has become popular of late. How many participants ran last year?": [
+            "10000 people",
+            "5000 people",
+            "60000 people",
+            "The city is too hilly for a marathon"
+        ],
+        "Explanation": "It has proven to be one of the most popular events in the athletics calendar in Europe"
+    },
+    {
+        "Which is the most famous street in Barcelona?": [
+            "Ramblas",
+            "Diagonal",
+            "Balmes",
+            "Aribau"
+        ],
+        "Explanation": "It's a beautiful promenade with live performers and flower shops. Enjoy your walk but mind your wallet there as pickpockets operate there"
+    },
+    {
+        "What's the full name of Barcelona's Airport?": [
+            "Barcelona El Prat",
+            "Barcelona Leo Messi",
+            "Barcelona",
+            "Barcelona Costa Brava"
+        ],
+        "Explanation": "In fact, the airport is not in the city but in a village nearby called El Prat"
+    },
+    {
+        "What distinctive color scheme do Barcelona taxis have?": [
+            "Black and yellow",
+            "White with a red stripe",
+            "Black",
+            "All taxis have been replaced by Uber vehicles, so the answer is any color"
+        ],
+        "Explanation": "You may not think they're pretty but they are super easy to spot"
+    },
+    {
+        "Which fashion brand stems from Barcelona?": [
+            "Desigual",
+            "Zara",
+            "Mango",
+            "Uniqlo"
+        ],
+        "Explanation": "If you stroll by the sea side, you will see its headquarters near the double u Hotel"
+    },
+    {
+        "Which river is not in the region of Barcelona?": [
+            "Duero",
+            "Llobregat",
+            "Besos",
+            "Ter"
+        ],
+        "Explanation": "The Duero is in North-West Spain"
+    },
+    {
+        "There's an important Picasso museum in Barcelona, but where was he from?": [
+            "Malaga",
+            "Madrid",
+            "Seville",
+            "Cuernavaca"
+        ],
+        "Explanation": "He grew up in Barcelona because his father was an arts teacher there, but the family stems from Malaga. Later he moved to Paris in France, where he became world famous"
+    }
+];
